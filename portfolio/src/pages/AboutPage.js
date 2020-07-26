5b86445b32231d718da69bfd8a762afdb2579cc5
Victor Picaud <victor.picaud@epitech.eu>
2020-07-26 17:10:09 +0200

    About Page setted up!

--- portfolio/src/pages/AboutPage.js
+++ portfolio/src/pages/AboutPage.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import Hero from '../components/Hero';
+import Content from '../components/Content';
+
 function AboutPage(props) {
 
 
   return (
-    <p>About Works!</p>
+    <div>
+      <Hero title={props.title} subTitle={props.subTitle}/>
+
+      <Content>
+        About Leo Lemberger here
+        Tell us about ya!!!!!
+      </Content>
+     </div>
   );
 }
 
